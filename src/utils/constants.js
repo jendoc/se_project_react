521e4b695e007caf4ebf436c2e4f44718208d87d
jendoc <jaharrisnv@gmail.com>
2023-02-20 16:16:36 -0800

    fix: making lint happy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,4 +20,4 @@ export function useForm(inputValues) {
 
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export { location, APIKey, baseUrl, headers, emailRegex };
+export { location, APIKey, headers, emailRegex };
